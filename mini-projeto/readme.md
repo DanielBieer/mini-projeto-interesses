c04ed62a92aa74002adb6c4fce61c139531aa5be
DanielBieer <danibierdasilva@gmail.com>
2024-06-29 02:43:33 -0300

    Mini projeto

---
+++ mini-projeto/readme.md
@@ -0,0 +1,13 @@
+## Função do software:
+Uma lista de interesses do usuário, também mostra uma notícia em destaque do IBGE.
+
+## Melhorias feitas:
+- Validação no botão de adicionar que impede a adição de interesses iguais;
+- Possibilidade do uso da tecla Enter ao invés de diretamente clicar no botão de adicionar;
+
+## Melhorias que poderiam ser feitas:
+- Atualização dinâmica da notícia em tempo real;
+- Adição de uma opção para excluir um único interesse;
+
+## Como executar:
+Após obter o código fonte, adquira o link HTML ou utilize extensões como "liveserver" para testar a lista.
---
+++ mini-projeto/template-mini-projeto/index.js
@@ -0,0 +1,93 @@
+/** Implemente seu código abaixo. Boa sorte! */
+
+const meusInteresses = document.querySelector('.meus-interesses');
+const inputInteresse = document.querySelector('#novo-interesse');
+
+// Função de atualizar e carregar a lista 
+function carregarInteresses() {
+    const interesses = localStorage.getItem('meus-interesses');
+
+    if (interesses) {
+        const listaInteresses = JSON.parse(interesses);
+
+        const ul = document.querySelector('ul');
+        ul.innerHTML = '';
+
+        listaInteresses.forEach(interesse => {
+            const li = document.createElement('li');
+            li.textContent = interesse;
+            ul.appendChild(li);
+        });
+    }
+}
+// Função de evento para adicionar novos interesses
+function buttonAdd() {
+    const novoInteresse = inputInteresse.value;
+    if (novoInteresse) {
+        let listaInteresses = localStorage.getItem('meus-interesses');
+
+        if (listaInteresses) {
+            listaInteresses = JSON.parse(listaInteresses);
+        } else {
+            listaInteresses = [];
+        }
+
+        
+        if (listaInteresses.includes(novoInteresse)) {
+            alert('Interesse já adicionado na lista.')
+        } else {
+        listaInteresses.push(novoInteresse);
+        localStorage.setItem('meus-interesses', JSON.stringify(listaInteresses));
+
+        carregarInteresses();
+        }
+
+        inputInteresse.value = '';
+    }
+}
+
+// Limpar a lista
+function clearList() {
+    localStorage.removeItem('meus-interesses');
+
+    const ul = document.querySelector('ul');
+    ul.innerHTML = '';
+
+    carregarInteresses();
+}
+
+carregarInteresses();
+
+setInterval(carregarInteresses, 1000);
+
+inputInteresse.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter'){
+        buttonAdd()
+    }
+})
+
+
+// Função para buscar notícias do IBGE
+async function fetchNoticiasIBGE() {
+    const url = 'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release';
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.items && data.items.length > 0) {
+            const primeiraNoticia = data.items[0];
+            const titulo = primeiraNoticia.titulo;
+
+            
+
+            const tituloNoticiaElement = document.querySelector('.title-news-today');
+            tituloNoticiaElement.textContent = titulo;
+        }
+    } catch (error) {
+        console.error('Erro ao buscar notícias do IBGE:', error);
+    }
+}
+
+
+fetchNoticiasIBGE();
